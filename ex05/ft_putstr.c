298a900b7c41e843328dd9884056305379160258
Elie VATINE <evatine@e3r4p5.42.fr>
2017-03-14 20:32:02 +0100

    check des normes jusqu'au 6

--- ex05/ft_putstr.c
+++ ex05/ft_putstr.c
@@ -6,30 +6,33 @@
 /*   By: evatine <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/13 15:17:43 by evatine           #+#    #+#             */
-/*   Updated: 2017/03/13 16:28:08 by evatine          ###   ########.fr       */
+/*   Updated: 2017/03/14 20:30:32 by evatine          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 #include <unistd.h>
+
 void	ft_putchar(char c)
 {
-		write( 1, &c , 1);
+	write(1, &c, 1);
 }
+
 void	ft_putstr(char *str)
 {
-		int		i;
-		char	c;
+	int		i;
+	char	c;
 
-		i = 0;
-		while (str[i])
-		{
-			ft_putchar(str[i]);
-			c = (*str + i);
-			i++;
-		}
+	i = 0;
+	while (str[i])
+	{
+		ft_putchar(str[i]);
+		c = (*str + i);
+		i++;
+	}
 }
 
-int	main()
+int		main(void)
 {
 	ft_putstr("Falafel");
-	return(0);
+	return (0);
 }
