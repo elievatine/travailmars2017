41e62312e722bcd9fb10715810b83b5090255575
Elie VATINE <evatine@e3r5p3.42.fr>
2017-03-15 16:31:45 +0100

    ex00j04

--- j04/ex00/ft_iterative_factorial.c
+++ j04/ex00/ft_iterative_factorial.c
@@ -6,11 +6,12 @@
 /*   By: evatine <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/15 16:02:14 by evatine           #+#    #+#             */
-/*   Updated: 2017/03/15 16:09:23 by evatine          ###   ########.fr       */
+/*   Updated: 2017/03/15 16:29:15 by evatine          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include <stdio.h>
 
-int		ft_iterative_factorial(int nb)
+int		ft_iterative_factorial(int nombre)
 {
 	int		factoriel;
 	int		x;
@@ -20,7 +21,13 @@ int		ft_iterative_factorial(int nb)
 	while (x < (nombre - 1))
 	{
 		factoriel = factoriel * (nombre - 1);
-		i++;
+		x++;
 	}
 	return (factoriel);
+}
+
+int		main(void)
+{
+	printf("%d", ft_iterative_factorial(5));
+	return(0);
 }
