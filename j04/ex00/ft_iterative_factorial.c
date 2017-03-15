cb37beda0cd5be8b144e54e493ea0d57a58c5313
Elie VATINE <evatine@e3r5p3.42.fr>
2017-03-15 16:34:37 +0100

    ajout du nb comme demande

--- j04/ex00/ft_iterative_factorial.c
+++ j04/ex00/ft_iterative_factorial.c
@@ -6,21 +6,21 @@
 /*   By: evatine <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/15 16:02:14 by evatine           #+#    #+#             */
-/*   Updated: 2017/03/15 16:29:15 by evatine          ###   ########.fr       */
+/*   Updated: 2017/03/15 16:33:57 by evatine          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <stdio.h>
 
-int		ft_iterative_factorial(int nombre)
+int		ft_iterative_factorial(int nb)
 {
 	int		factoriel;
 	int		x;
 
 	x = 1;
-	factoriel = nombre;
-	while (x < (nombre - 1))
+	factoriel = nb;
+	while (x < (nb - 1))
 	{
-		factoriel = factoriel * (nombre - 1);
+		factoriel = factoriel * (nb - 1);
 		x++;
 	}
 	return (factoriel);
