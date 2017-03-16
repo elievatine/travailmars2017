f67474c93a06c46ffb71d00c02055be1c1a48ce1
Elie VATINE <evatine@e3r2p3.42.fr>
2017-03-16 19:53:05 +0100

    deuxpreimiers du j09

---
+++ j09/ex01/ft_takes_place.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_takes_place.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: evatine <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/03/16 18:08:57 by evatine           #+#    #+#             */
+/*   Updated: 2017/03/16 19:51:22 by evatine          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+void	ft_takes_place(int hour)
+{
+	int		y;
+	int		x;
+	int		z;
+
+	z = hour - 11;
+	y = hour + 1;
+	x = hour - 12;
+	if (hour == 0)
+		printf("THE FOLLOWING TAKES PLACE BETWEEN 12.00 P.M AND 1.00 A.M\n");
+	if (hour == 12)
+		printf("THE FOLLOWING TAKES PLACE BETWEEN 12.00 A.M AND 1.00 P.M\n");
+	if (hour == 11)
+		printf("THE FOLLOWING TAKES PLACE BETWEEN 11.00 A.M AND 12.00 P.M\n");
+	if (hour == 23)
+		printf("THE FOLLOWING TAKES PLACE BETWEEN 11.00 P.M AND 12.00 A.M\n");
+	if (hour >= 1 && hour < 11)
+	{
+		printf("THE FOLLOWING TAKES PLACE BETWEEN %i,00 ", hour);
+		printf("A.M AND %i.00 A.M", y);
+	}
+	if (hour >= 13 && hour < 23)
+	{
+		printf("THE FOLLOWING TAKES PLACE BETWEEN %i,00 ", x);
+		printf("P.M AND %i.00 P.M", x + 1);
+	}
+}
