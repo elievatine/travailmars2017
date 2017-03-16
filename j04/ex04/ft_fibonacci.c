e2cbb3e4de9da47df5cd228b1b710f10dbee65d4
Elie VATINE <evatine@e3r2p3.42.fr>
2017-03-16 14:50:04 +0100

    ex04

---
+++ j04/ex04/ft_fibonacci.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_fibonacci.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: evatine <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/03/16 13:41:43 by evatine           #+#    #+#             */
+/*   Updated: 2017/03/16 14:41:53 by evatine          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int		ft_fibonacci(int index)
+{
+	int i;
+
+	i= 0;
+	if (index == 1)
+		return (1);
+	if (index == 2)
+		return (1);
+
+	if (index < 0)
+		return(-1);
+	else
+	{
+		i = ft_fibonacci(index - 1) + ft_fibonacci(index - 2);
+	return (i);
+	}
+}
+
+int		main(void)
+{
+	printf("%d", ft_fibonacci(7));
+
+	return(0);
+}
