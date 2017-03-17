917841fd40dbcb1a6e43fdeea593675de16a7c55
Elie VATINE <evatine@e3r2p3.42.fr>
2017-03-17 15:14:32 +0100

    ex05

--- j04/ex05/ft_sqrt.c
+++ j04/ex05/ft_sqrt.c
@@ -6,7 +6,7 @@
 /*   By: evatine <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/16 14:51:38 by evatine           #+#    #+#             */
-/*   Updated: 2017/03/16 15:03:50 by evatine          ###   ########.fr       */
+/*   Updated: 2017/03/17 15:14:03 by evatine          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,20 +15,23 @@
 int		ft_sqrt(int nb)
 {
 	int		i;
+	int		square;
 
 	i = 0;
-	while (nb > (1 * i))
+	square = 0;
+	while (i < nb / 2)
 	{
+		square = i * i;
+		if (square == nb)
+			return (i);
 		i++;
 	}
-	if ((nb % i) != 0)
-		i = 0;
-	return (i);
+	return (-1);
 }
-
+		
 int		main(void)
 {
-	printf("%d", ft_sqrt(8));
+	printf("%d", ft_sqrt(9));
 
 	return(0);
 }
