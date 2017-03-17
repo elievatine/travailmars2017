df722c6661c68a858ee01e09e5199e0f224b05a2
Elie VATINE <evatine@e3r2p3.42.fr>
2017-03-17 11:16:35 +0100

    modif

--- j09/ex04/ft_rot42.c
+++ j09/ex04/ft_rot42.c
@@ -6,21 +6,21 @@
 /*   By: evatine <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/16 21:39:42 by evatine           #+#    #+#             */
-/*   Updated: 2017/03/16 22:12:37 by evatine          ###   ########.fr       */
+/*   Updated: 2017/03/17 11:11:36 by evatine          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 char	*ft_rot42(char *str)
 {
 	int		i;
-	
+
 	i = 0;
 	while (str[i])
 	{
 		if (str[i] >= 65 && str[i] <= 90)
 		{
-			str[i] = str [i] -65;
-			str[i] = ((str[i] +42) % 26);
+			str[i] = str[i] - 65;
+			str[i] = ((str[i] + 42) % 26);
 			str[i] = str[i] + 65;
 		}
 		else if (str[i] >= 97 && str[i] <= 122)
