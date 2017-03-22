45849e9d2a957c62e03dd454cb11db0a3cec52a4
Elie VATINE <evatine@e3r4p2.42.fr>
2017-03-22 13:36:57 +0100

    j06

---
+++ j06/ex01/ft_print_program_name.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_program_name.c                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: evatine <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/03/22 10:10:38 by evatine           #+#    #+#             */
+/*   Updated: 2017/03/22 10:19:48 by evatine          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_putchar(char c);
+
+void	ft_putstr(char *str)
+{
+	int		i;
+
+	i = 0;
+	while (str[i])
+	{
+		ft_putchar(str[i]);
+		i++;
+	}
+}
+
+int		main(int argc, char **argv)
+{
+	if (argc == 0)
+	ft_putstr(argv[0]);
+	return (0);
+}
